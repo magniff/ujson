3a5f9921857409b3b3e9c4d82f82d6395fa58c53
magniff <tinysnippets@gmail.com>
2024-06-07 18:30:36 -0700

    video 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+#![allow(dead_code)]
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 struct State {
     current: usize,
 }
 
-#[derive(Debug, Clone, Copy, thiserror::Error)]
+#[derive(Debug, Clone, Copy, thiserror::Error, PartialEq, Eq)]
 enum ParserError {
     #[error("Parse error at position {0}")]
     NoParse(usize),
@@ -74,103 +76,269 @@ fn pure<'input, R: 'input>(value: R) -> Parser<'input, R> {
     }
 }
 
-fn string<'input>() -> Parser<'input, &'input str> {
+fn pure_fail<'input, R: 'input>(unwind: Option<usize>) -> Parser<'input, R> {
+    Parser {
+        inner: Box::new(move |_: &'input str, state| {
+            Err(ParserError::NoParse(
+                unwind.map_or(state.current, |u| state.current - u),
+            ))
+        }),
+    }
+}
+
+fn string<'input>() -> Parser<'input, JsonValue<'input>> {
     bind(pattern("\""), |_: &str| {
         bind(take_while(|c| c != '"'), |s| {
-            bind(pattern("\""), move |_: &str| pure(s))
+            bind(pattern("\""), move |_: &str| pure(JsonValue::String(s)))
         })
     })
 }
 
-fn number<'input>() -> Parser<'input, i64> {
+fn merge_two_consecutive_strs<'input>(s1: &'input str, s2: &'input str) -> &'input str {
+    unsafe {
+        std::str::from_utf8_unchecked(std::slice::from_raw_parts(s1.as_ptr(), s1.len() + s2.len()))
+    }
+}
+
+fn whole_part_number<'input>() -> Parser<'input, &'input str> {
     bind(or(pattern("-"), pattern("")), |sign| {
-        bind(take_while(|c| c.is_digit(10)), move |digits| {
-            pure(
-                unsafe {
-                    std::str::from_utf8_unchecked(std::slice::from_raw_parts(
-                        sign.as_ptr(),
-                        sign.len() + digits.len(),
-                    ))
+        bind(take_while(|c| c.is_digit(10)), |digits| {
+            match digits.len() {
+                0 => pure_fail(Some(sign.len())),
+                1 => pure(merge_two_consecutive_strs(sign, digits)),
+                other if digits.chars().nth(0).unwrap() == '0' => {
+                    pure_fail(Some(other + sign.len()))
                 }
-                .parse::<i64>()
-                .unwrap(),
-            )
+                _ => pure(merge_two_consecutive_strs(sign, digits)),
+            }
         })
     })
 }
 
-// test the pattern function
-#[test]
-fn test_pattern() {
-    let parser = pattern("hello");
-    let input = "hello world";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, ("hello", State { current: 5 }));
+fn decimal_part_number<'input>() -> Parser<'input, &'input str> {
+    bind(pattern("."), |dot: &str| {
+        bind(take_while(|c| c.is_digit(10)), |digits| {
+            pure(merge_two_consecutive_strs(dot, digits))
+        })
+    })
 }
 
-// test the or function
-#[test]
-fn test_or() {
-    let parser = or(pattern("hello"), pattern("world"));
-    let input = "world";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, ("world", State { current: 5 }));
+fn optional<'input, R: 'input>(parser: Parser<'input, R>) -> Parser<'input, Option<R>> {
+    Parser {
+        inner: Box::new(
+            move |input: &'input str, state| match (parser.inner)(input, state) {
+                Ok((result, new_state)) => Ok((Some(result), new_state)),
+                Err(_) => Ok((None, state)),
+            },
+        ),
+    }
 }
 
-// test the take_while function
-#[test]
-fn test_take_while() {
-    let parser = take_while(|c| c.is_alphabetic());
-    let input = "hello world";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, ("hello", State { current: 5 }));
+fn number<'input>() -> Parser<'input, JsonValue<'input>> {
+    bind(whole_part_number(), |whole_part| {
+        bind(
+            optional(decimal_part_number()),
+            move |decimal_part| match decimal_part {
+                Some(decimal_part) => pure(JsonValue::Number(
+                    merge_two_consecutive_strs(whole_part, decimal_part)
+                        .parse::<f64>()
+                        .unwrap(),
+                )),
+                None => pure(JsonValue::Number(whole_part.parse::<f64>().unwrap())),
+            },
+        )
+    })
 }
 
-// test the pure function
-#[test]
-fn test_pure() {
-    let parser = pure("hello");
-    let input = "world";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, ("hello", State { current: 0 }));
+fn boolean<'input>() -> Parser<'input, JsonValue<'input>> {
+    or(
+        bind(pattern("true"), |_| pure(JsonValue::Boolean(true))),
+        bind(pattern("false"), |_| pure(JsonValue::Boolean(false))),
+    )
 }
 
-// test the bind parser using the pure parser
-#[test]
-fn test_bind() {
-    let parser = bind(pattern("hello"), |s| pure(s.to_uppercase()));
-    let input = "hello world";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, ("HELLO".to_string(), State { current: 5 }));
+fn null<'input>() -> Parser<'input, JsonValue<'input>> {
+    bind(pattern("null"), |_| pure(JsonValue::Null))
 }
 
-// test the string parser
-#[test]
-fn test_string() {
-    let parser = string();
-    let input = "\"hello\"";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, ("hello", State { current: 7 }));
+#[derive(Debug, Clone, PartialEq)]
+enum JsonValue<'input> {
+    String(&'input str),
+    Number(f64),
+    Object(std::collections::HashMap<&'input str, JsonValue<'input>>),
+    List(Vec<JsonValue<'input>>),
+    Boolean(bool),
+    Null,
 }
 
-// test the number parser
-#[test]
-fn test_number() {
-    let parser = number();
-    let input = "123";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, (123, State { current: 3 }));
+mod tests {
+    use super::*;
 
-    let parser = number();
-    let input = "-123";
-    let state = State { current: 0 };
-    let result = (parser.inner)(input, state).unwrap();
-    assert_eq!(result, (-123, State { current: 4 }));
+    // test the pattern function
+    #[test]
+    fn test_pattern() {
+        let parser = pattern("hello");
+        let input = "hello world";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("hello", State { current: 5 }));
+    }
+
+    // test the or function
+    #[test]
+    fn test_or() {
+        let parser = or(pattern("hello"), pattern("world"));
+        let input = "world";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("world", State { current: 5 }));
+    }
+
+    // test the take_while function
+    #[test]
+    fn test_take_while() {
+        let parser = take_while(|c| c.is_alphabetic());
+        let input = "hello world";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("hello", State { current: 5 }));
+    }
+
+    // test the pure function
+    #[test]
+    fn test_pure() {
+        let parser = pure("hello");
+        let input = "world";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("hello", State { current: 0 }));
+    }
+
+    // test the bind parser using the pure parser
+    #[test]
+    fn test_bind() {
+        let parser = bind(pattern("hello"), |s| pure(s.to_uppercase()));
+        let input = "hello world";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("HELLO".to_string(), State { current: 5 }));
+    }
+
+    // test the string parser
+    #[test]
+    fn test_string() {
+        let parser = string();
+        let input = "\"hello\"";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (JsonValue::String("hello"), State { current: 7 }));
+    }
+
+    // test the number parser
+    #[test]
+    fn test_number() {
+        let parser = number();
+        let input = "123";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (JsonValue::Number(123.0), State { current: 3 }));
+
+        let parser = number();
+        let input = "-123";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (JsonValue::Number(-123.0), State { current: 4 }));
+
+        let parser = number();
+        let input = "-00000000000001";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap_err();
+        assert_eq!(result, ParserError::NoParse(0));
+    }
+
+    // test the pure fail parser
+    #[test]
+    fn test_pure_fail() {
+        let parser = pure_fail::<i64>(None);
+        let input = "hello world";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap_err();
+        assert_eq!(result, ParserError::NoParse(0));
+
+        let parser = pure_fail::<i64>(Some(2));
+        let input = "hello world";
+        let state = State { current: 2 };
+        let result = (parser.inner)(input, state).unwrap_err();
+        assert_eq!(result, ParserError::NoParse(0));
+    }
+
+    // test the whole part number parser
+    #[test]
+    fn test_whole_part_number() {
+        let parser = whole_part_number();
+        let input = "123";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("123", State { current: 3 }));
+
+        let parser = whole_part_number();
+        let input = "-123";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("-123", State { current: 4 }));
+
+        let parser = whole_part_number();
+        let input = "0";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, ("0", State { current: 1 }));
+
+        let parser = whole_part_number();
+        let input = "-00000000000001";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap_err();
+        assert_eq!(result, ParserError::NoParse(0));
+    }
+
+    // test the decimal part number parser
+    #[test]
+    fn test_decimal_part_number() {
+        let parser = decimal_part_number();
+        let input = ".123";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (".123", State { current: 4 }));
+
+        let parser = decimal_part_number();
+        let input = ".00000000000001";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (".00000000000001", State { current: 15 }));
+    }
+
+    // test the boolean parser
+    #[test]
+    fn test_boolean() {
+        let parser = boolean();
+        let input = "true";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (JsonValue::Boolean(true), State { current: 4 }));
+
+        let parser = boolean();
+        let input = "false";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (JsonValue::Boolean(false), State { current: 5 }));
+    }
+
+    // test the null parser
+    #[test]
+    fn test_null() {
+        let parser = null();
+        let input = "null";
+        let state = State { current: 0 };
+        let result = (parser.inner)(input, state).unwrap();
+        assert_eq!(result, (JsonValue::Null, State { current: 4 }));
+    }
 }
